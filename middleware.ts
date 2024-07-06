54ec79da1d0e17c357703889cbaa946e71b867b2
Sharm Yparraguire <sharmyparraguire@equitytransport.com.au>
2024-07-07 01:59:18 +0800

    edge comp test

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,3 @@ import authConfig from "@/auth.config";
 import NextAuth from "next-auth";
 
 export const { auth: middleware } = NextAuth(authConfig);
-
-export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
-};
