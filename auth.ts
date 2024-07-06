76fd8f2f164d8d1be39b80762950ba79d32a6769
Sharm Yparraguire <sharmyparraguire@equitytransport.com.au>
2024-07-07 01:26:08 +0800

    edge comp test

--- auth.ts
+++ auth.ts
@@ -5,7 +5,12 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export const { handlers, signIn, signOut, auth } = NextAuth({
+export const {
+  handlers: { GET, POST },
+  signIn,
+  signOut,
+  auth,
+} = NextAuth({
   adapter: PrismaAdapter(prisma),
   session: { strategy: "jwt" },
   ...authConfig,
